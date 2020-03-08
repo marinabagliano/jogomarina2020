0598d5c17928f41ec9d39917da172876adc160b9
Marina Bagliano <marinabsbs10@gmail.com>
2020-03-08 08:38:48 -0300

    TouchableGameObject, Stats

---
+++ Assets/Scripts/NavGame/Core/Stats.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NavGame.Core
+{
+    [Serializable]
+
+    public class Stats
+    {
+        public int maxHealth = 100;
+        public int damage = 10;
+        public int armor = 1;
+        public float attackSeed = 1f;
+
+
+
+    }
+}
