1764aab77a7455fa2e29b571bd9430b697a10da8
Marina Bagliano <marinabsbs10@gmail.com>
2020-03-17 11:52:30 -0300

    Commit Provisorio - Dev4 aula10 minut 4:30

---
+++ Assets/Scripts/NavGame/Core/HealthUIController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NavGame.Core
+{
+     [RequireComponent(typeof(DamageableGameObject))]
+    public class HealthUIController : MonoBehaviour
+    {
+        public GameObject healthUIPrefab;
+        public Transform healthPosition;
+
+        void Awake()
+        {
+            Canvas canvas = FindWordCanvas();
+        }
+        
+        Canvas FindWordCanvas()
+        {
+            foreach (Canvas c in FindObjectOfType<Canvas>())
+            {
+                
+            }
+        }
+    }
+}
