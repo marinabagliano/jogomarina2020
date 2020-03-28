60558469563f35c516772ab529a24bc91b8f6e17
Marina Bagliano <marinabsbs10@gmail.com>
2020-03-28 17:06:08 -0300

    Health UI instantion

--- Assets/Scripts/NavGame/Core/HealthUIController.cs
+++ Assets/Scripts/NavGame/Core/HealthUIController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,18 +10,40 @@ namespace NavGame.Core
     {
         public GameObject healthUIPrefab;
         public Transform healthPosition;
+        GameObject healthUI;
+        Transform cam;
 
         void Awake()
         {
             Canvas canvas = FindWordCanvas();
+            if(canvas == null)
+            {
+                throw new Exception("A WordSpace canvas was needed");
+            }
+            cam = Camera.main.transform;
+            healthUI = Instantiate(healthUIPrefab, canvas.transform);
         }
-        
+
+        void LateUpdate()
+        {
+            if(healthUI != null)
+            {
+                healthUI.transform.position = healthPosition.position;
+                healthUI.transform.forward = -cam.forward;
+            }
+
+        }
+
         Canvas FindWordCanvas()
         {
-            foreach (Canvas c in FindObjectOfType<Canvas>())
+            foreach (Canvas c in FindObjectsOfType<Canvas>())
             {
-                
+                if(c.renderMode == RenderMode.WorldSpace)
+                {
+                    return c;
+                }
             }
+            return null;
         }
     }
 }
