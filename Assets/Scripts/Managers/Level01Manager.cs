94353fccffe1751292d5a89afdcb8bac14441a60
Marina Bagliano <marinabsbs10@gmail.com>
2020-05-07 10:28:04 -0300

    Action, abstract LevelManager

--- Assets/Scripts/Managers/Level01Manager.cs
+++ Assets/Scripts/Managers/Level01Manager.cs
@@ -1,21 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using NavGame.Managers;
 
-public class Level01Manager : MonoBehaviour
+public class Level01Manager : LevelManager
 {
     public Transform[] badSpawn;
     public GameObject badPrefab;
     public int badWaves = 3;
     public float waitTimeFirstWave = 2f ;
     public float waitTimeBetweenWaves = 4f;
-    void Start()
-    {
-        StartCoroutine(SpawnBad());
-        
-    }
+  
 
-    IEnumerator SpawnBad()
+    protected override IEnumerator SpawnBad()
     {
         yield return new WaitForSeconds(waitTimeFirstWave);
 
