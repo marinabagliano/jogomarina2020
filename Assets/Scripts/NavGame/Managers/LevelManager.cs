fb3c686d40eb6a5db082aac7f2f2d170118ad02a
Marina Bagliano <marinabsbs10@gmail.com>
2020-05-12 22:18:16 -0300

    Validating action cooldown

--- Assets/Scripts/NavGame/Managers/LevelManager.cs
+++ Assets/Scripts/NavGame/Managers/LevelManager.cs
@@ -11,7 +11,7 @@ namespace NavGame.Managers
     {
         public static LevelManager instance;
         public Action[] actions;
-
+        public string errorSound;
         public OnActionSelectEvent onActionSelect;
         public OnActionCancelEvent onActionCancel;
         public OnActionCooldownUpdateEvent onActionCooldownUpdate
@@ -36,7 +36,11 @@ namespace NavGame.Managers
 
         public virtual void SelectedAction(int actionIndex)
         {
-
+          if(actions[actionIndex].coolDown > 0)
+          {
+            AudioManager.instance.Play(errorSound, PlayerManager.instance.GetPlayer().transform.position);
+              return;
+          }
           CancelAction();
           selectedAction = actionIndex;
           if(onActionSelect != null)
